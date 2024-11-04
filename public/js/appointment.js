2c87989b0f2ac7484832c93b6f8fc1a07c15a0e8
Ichan-creator <chris.aquino188@gmail.com>
2024-11-05 01:19:27 +0800

    add files

--- public/js/appointment.js
+++ public/js/appointment.js
@@ -323,10 +323,10 @@ window.addEventListener("load", () => {
       then: (data) =>
         data.map((item) => [
           item.id,
-          dayjs(item.appointmentDate).format("MMMM DD, YYYY - hh:mm A"),
+          dayjs(item.appointmentDate).utc().format("MMMM DD, YYYY - hh:mm A"),
           item.service,
           item.dateApproved ? item.dateApproved !== "PENDING" 
-          ? dayjs(item.dateApproved).format("MMMM DD, YYYY - hh:mm A") : "PENDING" : "",
+          ? dayjs(item.dateApproved).utc().format("MMMM DD, YYYY - hh:mm A") : "PENDING" : "",
           null,
         ]),
       handle: (res) => {
