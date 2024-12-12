e63c01a77742a979d0ae83dbafaab10a8c7da558
Ichan-creator <chris.aquino188@gmail.com>
2024-12-12 17:34:07 +0800

    update

--- public/js/clientAppointmentHistory.js
+++ public/js/clientAppointmentHistory.js
@@ -1,3 +1,49 @@
+const showNavbar = (toggleId, navId, bodyId, headerId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId),
+    bodypd = document.getElementById(bodyId),
+    headerpd = document.getElementById(headerId);
+
+  if (toggle && nav && bodypd && headerpd) {
+    nav.addEventListener("mouseenter", () => {
+      if (window.innerWidth > 768) {
+        nav.classList.add("show");
+        toggle.classList.add("bx-x");
+        bodypd.classList.add("body-pd");
+        headerpd.classList.add("body-pd");
+      }
+    });
+
+    nav.addEventListener("mouseleave", () => {
+      if (window.innerWidth > 768) {
+        nav.classList.remove("show");
+        toggle.classList.remove("bx-x");
+        bodypd.classList.remove("body-pd");
+        headerpd.classList.remove("body-pd");
+      }
+    });
+
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+      toggle.classList.toggle("bx-x");
+      bodypd.classList.toggle("body-pd");
+      headerpd.classList.toggle("body-pd");
+    });
+  }
+};
+
+showNavbar("header-toggle", "nav-bar", "body-pd", "header");
+
+const linkColor = document.querySelectorAll(".nav__link");
+
+function colorLink() {
+  if (linkColor) {
+    linkColor.forEach((l) => l.classList.remove("active"));
+    this.classList.add("active");
+  }
+}
+linkColor.forEach((l) => l.addEventListener("click", colorLink));
+
 const { h } = window.gridjs;
 
 window.addEventListener("load", () => {
